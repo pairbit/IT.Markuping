62ff431fcdbbc48cf1dc25b5dcee74073a62df73
Ivan Tikhonov <itikhonov@mail.com>
2025-12-18 14:56:40 +0300

    add BytesEncoding

---
+++ IT.Markuping/Encodings/BytesEncoding.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IT.Markuping.Encodings;
+
+public class BytesEncoding
+{
+    private readonly struct Tokens
+    {
+        public static readonly Tokens Utf16 = new(
+            [(byte)'<', 0], [(byte)'>', 0], [(byte)'/', 0], [(byte)':', 0],
+            [(byte)' ', 0], [(byte)'"', 0], [(byte)'\'', 0], [(byte)'=', 0]);
+
+        public static readonly Tokens Utf16BE = new(
+            [0, (byte)'<'], [0, (byte)'>'], [0, (byte)'/'], [0, (byte)':'],
+            [0, (byte)' '], [0, (byte)'"'], [0, (byte)'\''], [0, (byte)'=']);
+
+        public static readonly Tokens Utf32 = new(
+            [(byte)'<', 0, 0, 0], [(byte)'>', 0, 0, 0], [(byte)'/', 0, 0, 0], [(byte)':', 0, 0, 0],
+            [(byte)' ', 0, 0, 0], [(byte)'"', 0, 0, 0], [(byte)'\'', 0, 0, 0], [(byte)'=', 0, 0, 0]);
+
+        public static readonly Tokens Utf32BE = new(
+            [0, 0, 0, (byte)'<'], [0, 0, 0, (byte)'>'], [0, 0, 0, (byte)'/'], [0, 0, 0, (byte)':'],
+            [0, 0, 0, (byte)' '], [0, 0, 0, (byte)'"'], [0, 0, 0, (byte)'\''], [0, 0, 0, (byte)'=']);
+
+        //60,62,47,58,32,34,39,61
+        public static readonly Tokens Utf8 = new(
+            [(byte)'<'], [(byte)'>'], [(byte)'/'], [(byte)':'],
+            [(byte)' '], [(byte)'"'], [(byte)'\''], [(byte)'=']);
+
+        public static readonly Tokens EBCDIC = new(
+            [76], [110], [97], [122], [64], [127], [125], [126]);
+
+        public static readonly Tokens EBCDIC_Turkish = new(
+            [76], [110], [97], [122], [64], [252], [125], [126]);
+
+        internal readonly byte[] _lt;
+        internal readonly byte[] _gt;
+        internal readonly byte[] _slash;
+        internal readonly byte[] _colon;
+        internal readonly byte[] _space;//" "
+        internal readonly byte[] _quot;//"
+        internal readonly byte[] _apos;//'
+        internal readonly byte[] _eq;//=
+
+        public Tokens(byte[] lt, byte[] gt, byte[] slash, byte[] colon,
+            byte[] space, byte[] quot, byte[] apos, byte[] eq)
+        {
+            _lt = lt;
+            _gt = gt;
+            _slash = slash;
+            _colon = colon;
+            _space = space;
+            _quot = quot;
+            _apos = apos;
+            _eq = eq;
+        }
+    }
+
+    #region Private Fields
+
+    /// <summary>
+    /// &lt;
+    /// </summary>
+    internal readonly byte[] _lt;
+
+    /// <summary>
+    /// &gt;
+    /// </summary>
+    internal readonly byte[] _gt;
+
+    /// <summary>
+    /// /
+    /// </summary>
+    internal readonly byte[] _slash;
+
+    /// <summary>
+    /// :
+    /// </summary>
+    internal readonly byte[] _colon;
+
+    internal readonly byte[] _space;
+
+    /// <summary>
+    /// "
+    /// </summary>
+    internal readonly byte[] _quot;
+
+    /// <summary>
+    /// '
+    /// </summary>
+    internal readonly byte[] _apos;
+
+    /// <summary>
+    /// =
+    /// </summary>
+    internal readonly byte[] _eq;
+
+    internal readonly byte[][] _otherSpaces;
+
+    /// <summary>
+    /// &lt;/
+    /// </summary>
+    internal readonly byte[] _startClosing;
+
+    /// <summary>
+    /// /&gt;
+    /// </summary>
+    internal readonly byte[] _selfClosing;
+
+    internal readonly int _minLength;
+
+    #endregion Private Fields
+
+    private BytesEncoding(Tokens tokens, byte[][]? otherSpaces = null)
+        : this(tokens._lt, tokens._gt, tokens._slash, tokens._colon,
+              tokens._space, tokens._quot, tokens._apos, tokens._eq,
+              otherSpaces)
+    {
+    }
+
+    public BytesEncoding(byte[] lt, byte[] gt, byte[] slash, byte[] colon,
+        byte[] space, byte[] quot, byte[] apos, byte[] eq,
+        byte[][]? otherSpaces = null)
+    {
+        _lt = lt;
+        _gt = gt;
+        _slash = slash;
+        _colon = colon;
+        _space = space;
+        _quot = quot;
+        _apos = apos;
+        _eq = eq;
+
+        _minLength = Math.Min(lt.Length, gt.Length);
+        _minLength = Math.Min(_minLength, slash.Length);
+        _minLength = Math.Min(_minLength, colon.Length);
+        _minLength = Math.Min(_minLength, space.Length);
+        _minLength = Math.Min(_minLength, quot.Length);
+        _minLength = Math.Min(_minLength, apos.Length);
+        _minLength = Math.Min(_minLength, eq.Length);
+
+        if (otherSpaces != null)
+        {
+            _otherSpaces = otherSpaces;
+            foreach (var otherSpace in otherSpaces)
+            {
+                _minLength = Math.Min(_minLength, otherSpace.Length);
+            }
+        }
+        else
+        {
+            _otherSpaces = [];
+        }
+
+        _startClosing = new byte[lt.Length + slash.Length];
+        lt.AsSpan().CopyTo(_startClosing);
+        slash.AsSpan().CopyTo(_startClosing.AsSpan(lt.Length));
+
+        _selfClosing = new byte[slash.Length + gt.Length];
+        slash.AsSpan().CopyTo(_selfClosing);
+        gt.AsSpan().CopyTo(_selfClosing.AsSpan(slash.Length));
+    }
+
+    public static readonly BytesEncoding Utf16_Strict = new(Tokens.Utf16);
+    public static readonly BytesEncoding Utf16BE_Strict = new(Tokens.Utf16BE);
+    public static readonly BytesEncoding Utf32_Strict = new(Tokens.Utf32);
+    public static readonly BytesEncoding Utf32BE_Strict = new(Tokens.Utf32BE);
+
+    public static readonly BytesEncoding Utf16 = new(Tokens.Utf16, [[10, 0], [13, 0], [9, 0]]);
+    public static readonly BytesEncoding Utf16BE = new(Tokens.Utf16BE, [[0, 10], [0, 13], [0, 9]]);
+    public static readonly BytesEncoding Utf32 = new(Tokens.Utf32, [[10, 0, 0, 0], [13, 0, 0, 0], [9, 0, 0, 0]]);
+    public static readonly BytesEncoding Utf32BE = new(Tokens.Utf32BE, [[0, 0, 0, 10], [0, 0, 0, 13], [0, 0, 0, 9]]);
+
+    public static bool TryGet(int codePage, [MaybeNullWhen(false)] out BytesEncoding bytesEncoding)
+    {
+        if (codePage == 1200)
+        {
+            bytesEncoding = Utf16;
+            return true;
+        }
+        if (codePage == 1201)
+        {
+            bytesEncoding = Utf16BE;
+            return true;
+        }
+        if (codePage == 12000)
+        {
+            bytesEncoding = Utf32;
+            return true;
+        }
+        if (codePage == 12001)
+        {
+            bytesEncoding = Utf32BE;
+            return true;
+        }
+        bytesEncoding = null;
+        return false;
+    }
+
+    public static bool TryGetStrict(int codePage, [MaybeNullWhen(false)] out BytesEncoding bytesEncoding)
+    {
+        if (codePage == 1200)
+        {
+            bytesEncoding = Utf16_Strict;
+            return true;
+        }
+        if (codePage == 1201)
+        {
+            bytesEncoding = Utf16BE_Strict;
+            return true;
+        }
+        if (codePage == 12000)
+        {
+            bytesEncoding = Utf32_Strict;
+            return true;
+        }
+        if (codePage == 12001)
+        {
+            bytesEncoding = Utf32BE_Strict;
+            return true;
+        }
+        bytesEncoding = null;
+        return false;
+    }
+}
