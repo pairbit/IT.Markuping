758a7993c1e24959f4741c4b04f6c7096730893d
Ivan Tikhonov <itikhonov@mail.com>
2025-12-18 17:59:35 +0300

    add Tester

--- IT.Markuping.Tests/TagFinder/TagFinderByteTester.cs
+++ IT.Markuping.Tests/TagFinder/TagFinderByteTester.cs
@@ -1,211 +1,32 @@
-﻿using IT.Markuping.Implementation;
-using IT.Markuping.Interfaces;
+﻿using IT.Markuping.Interfaces;
 using System;
 using System.Linq;
 using System.Text;
 
 namespace IT.Markuping.Tests;
 
-internal class TagFinderByteTest
+internal class TagFinderByteTester
 {
-    [Test]
-    public void Tester()
+    private readonly ITagFinder<byte> _finder;
+    private readonly Encoding _encoding;
+
+    public TagFinderByteTester(ITagFinder<byte> finder, Encoding encoding)
     {
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-        var encodingInfos = Encoding.GetEncodings();
-
-        Console.WriteLine($"encodingInfos count: {encodingInfos.Length}");
-
-        foreach (var encodingInfo in encodingInfos)
-        {
-            var codePage = encodingInfo.CodePage;
-
-            if (TagFinderByte.TryGet(codePage, out var tagFinder))
-            {
-                var encoding = encodingInfo.GetEncoding();
-                var tester = new TagFinderByteTester(tagFinder, encoding);
-
-                tester.Test();
-            }
-            else
-            {
-                Assert.Fail($"{codePage,5} Options not supported");
-            }
-        }
+        _finder = finder;
+        _encoding = encoding;
     }
 
-    [Test]
-    public void FailTest()
+    public void Test()
     {
-        var bytes = "------</Sig>98989<Sig>-----"u8;
-        var name = "Sig"u8;
-
-        var wrap = TagFinderByte.Utf8.WrapPair(bytes, name, out _);
-        Assert.That(wrap.IsEmpty, Is.True);
-
-        bytes = "----<Sig/"u8;
-        var opening = TagFinderByte.Utf8.Last(bytes, name);
-        Assert.That(opening.IsEmpty, Is.True);
-
-        bytes = Encoding.Unicode.GetBytes("------</Sig>98989<Sig>-----");
-        name = Encoding.Unicode.GetBytes("Sig");
-
-        wrap = TagFinderByte.Utf16.WrapPair(bytes, name, out _);
-        Assert.That(wrap.IsEmpty, Is.True);
+        LastClosingTest();
+        FirstTest();
     }
 
-    [Test]
-    public void First()
+    public void FirstTest()
     {
-        FirstTest(TagFinderByte.Utf32, Encoding.UTF32);
+        var encoding = _encoding;
+        var finder = _finder;
 
-        FirstTest(TagFinderByte.Utf16, Encoding.Unicode);
-
-        FirstTest(TagFinderByte.Utf8, Encoding.UTF8);
-    }
-
-    [Test]
-    public void LastClosing()
-    {
-        LastClosingTest(TagFinderByte.Utf32, Encoding.UTF32);
-
-        LastClosingTest(TagFinderByte.Utf16, Encoding.Unicode);
-
-        LastClosingTest(TagFinderByte.Utf8, Encoding.UTF8);
-    }
-
-    [Test]
-    public void BaseTest()
-    {
-        var str = "<a /><a b='\"' c=\">\">123</a><a />";
-        var nameStr = "a";
-
-        var outerStr = "<a b='\"' c=\">\">123</a>";
-        var innerStr = "123";
-
-        var bytes = Encoding.UTF8.GetBytes(str);
-        var name = Encoding.UTF8.GetBytes(nameStr);
-        var outer = Encoding.UTF8.GetBytes(outerStr);
-        var inner = Encoding.UTF8.GetBytes(innerStr);
-
-        var wrap = TagFinderByte.Utf8.WrapPair(bytes, name, out _);
-        Assert.That(wrap.HasAttributes, Is.True);
-        Assert.That(bytes[wrap.Outer].SequenceEqual(outer), Is.True);
-        Assert.That(bytes[wrap.Inner].SequenceEqual(inner), Is.True);
-
-        var last = TagFinderByte.Utf8.LastPair(bytes, name, out _);
-        Assert.That(wrap.HasAttributes, Is.True);
-        Assert.That(bytes[last.Outer].SequenceEqual(outer), Is.True);
-        Assert.That(bytes[last.Inner].SequenceEqual(inner), Is.True);
-
-        bytes = Encoding.Unicode.GetBytes(str);
-        name = Encoding.Unicode.GetBytes(nameStr);
-        outer = Encoding.Unicode.GetBytes(outerStr);
-        inner = Encoding.Unicode.GetBytes(innerStr);
-
-        //range = TagFinderBytes.Utf16.Outer(bytes, name);
-        //Assert.That(bytes[range].SequenceEqual(outer), Is.True);
-
-        //range = TagFinderBytes.Utf16.Inner(bytes, name);
-        //Assert.That(bytes[range].SequenceEqual(inner), Is.True);
-    }
-
-    [Test]
-    public void AllEncodings()
-    {
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-        var encodingInfos = Encoding.GetEncodings();
-
-        Console.WriteLine($"encodingInfos count: {encodingInfos.Length}");
-
-        foreach (var encodingInfo in encodingInfos)
-        {
-            var codePage = encodingInfo.CodePage;
-            var encoding = encodingInfo.GetEncoding();
-
-            //if (TagFinder.TryGetStrict(codePage, out var tagFinder))
-            //{
-            //    FullTest(tagFinder, encoding, "\0 ", false);
-            //    FullTest(tagFinder, encoding, "\0 ", true);
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"{codePage,5} Strict Options not supported");
-            //}
-
-            if (TagFinderByte.TryGet(codePage, out var tagFinder))
-            {
-                FullTest(tagFinder, encoding, "\0\t\n\r ", false);
-                FullTest(tagFinder, encoding, "\0\t\n\r ", true);
-            }
-            else
-            {
-                Console.WriteLine($"{codePage,5} Options not supported");
-            }
-        }
-    }
-
-    private static void FullTest(ITagFinder<byte> tagFinder, Encoding encoding, string spaces, bool hasNamespace)
-    {
-        var nameBytes = encoding.GetBytes("Signature");
-
-        foreach (var space in spaces)
-        {
-            try
-            {
-                var str = GetSignature2(space, hasNamespace);
-                var bytes = encoding.GetBytes("Signature" + str + "Signature");
-                var lastInner = "last";
-
-                var last = tagFinder.LastPair(bytes, nameBytes, out _);
-                Assert.That(last.HasNamespace, Is.EqualTo(hasNamespace));
-                Assert.That(bytes[last.Outer].SequenceEqual(encoding.GetBytes(GetSignature(space, hasNamespace, lastInner))), Is.True);
-                Assert.That(bytes[last.Inner].SequenceEqual(encoding.GetBytes(lastInner)), Is.True);
-
-                var wrap = tagFinder.WrapPair(bytes, nameBytes, out _);
-                Assert.That(bytes[wrap.Outer].SequenceEqual(encoding.GetBytes(str)), Is.True);
-                Assert.That(bytes[wrap.Inner].SequenceEqual(encoding.GetBytes(GetSignatureInner(space, hasNamespace))), Is.True);
-            }
-            catch
-            {
-                Console.WriteLine($"CodePage: {encoding.CodePage}");
-                Console.WriteLine($"space: '{space}'");
-                Console.WriteLine($"hasNamespace: {hasNamespace}");
-                throw;
-            }
-        }
-    }
-
-    private static string GetSignature2(char space, bool hasNamespace) =>
-        GetSignature(space, hasNamespace, "first") +
-        GetSignature(space, hasNamespace, "last");
-
-    private static string GetSignatureInner(char space, bool hasNamespace)
-    {
-        if (space == '\0') return hasNamespace
-                ? "first</ds:Signature><ds:Signature>last"
-                : "first</Signature><Signature>last";
-
-        return hasNamespace
-            ? $"first</ds:Signature{space}><ds:Signature{space}a='\"/>' b=\"'>1\" c=3>last"
-            : $"first</Signature{space}><Signature{space}a='\"/>' b=\"'>1\" c=3>last";
-    }
-
-    private static string GetSignature(char space, bool hasNamespace, string inner)
-    {
-        if (space == '\0') return hasNamespace
-                ? $"<ds:Signature>{inner}</ds:Signature>"
-                : $"<Signature>{inner}</Signature>";
-
-        return hasNamespace
-            ? $"<ds:Signature{space}a='\"/>' b=\"'>1\" c=3>{inner}</ds:Signature{space}>"
-            : $"<Signature{space}a='\"/>' b=\"'>1\" c=3>{inner}</Signature{space}>";
-    }
-
-    private static void FirstTest(ITagFinder<byte> finder, Encoding encoding)
-    {
         var name = encoding.GetBytes("a");
         var n = encoding.GetBytes("n");
         var bytes = encoding.GetBytes("<a/>").AsSpan();
@@ -288,8 +109,11 @@ internal class TagFinderByteTest
         Assert.That(finder.First(encoding.GetBytes("<a \">"), name).IsEmpty, Is.True);
     }
 
-    private static void LastClosingTest(ITagFinder<byte> finder, Encoding encoding)
+    public void LastClosingTest()
     {
+        var encoding = _encoding;
+        var finder = _finder;
+
         var name = encoding.GetBytes("a");
         var n = encoding.GetBytes("n");
         var bytes = encoding.GetBytes("</a>").AsSpan();
