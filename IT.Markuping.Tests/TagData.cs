535a7750ea8a19202ea55bcfac957d528c0f4a73
Ivan Tikhonov <itikhonov@mail.com>
2025-12-21 18:03:27 +0300

    refact tests

---
+++ IT.Markuping.Tests/TagData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace IT.Markuping.Tests;
+
+internal class TagData
+{
+    private byte[] NameBytes { get; }
+
+    private byte[] NamespaceBytes { get; }
+
+    private byte[] FullNameBytes { get; }
+
+    public string NameString { get; }
+
+    public string NamespaceString { get; }
+
+    public string FullNameString { get; }
+
+    public ReadOnlySpan<byte> Name => NameBytes;
+
+    public ReadOnlySpan<byte> Namespace => NamespaceBytes;
+
+    public ReadOnlySpan<byte> FullName => FullNameBytes;
+
+    public bool HasNamespace => NamespaceString.Length > 0;
+
+    public TagData(Encoding encoding, string name)
+    {
+        FullNameString = NameString = name;
+        FullNameBytes = NameBytes = encoding.GetBytes(name);
+        NamespaceString = string.Empty;
+        NamespaceBytes = [];
+    }
+
+    public TagData(Encoding encoding, string name, string nameSpace)
+    {
+        NameString = name;
+        NamespaceString = nameSpace;
+        FullNameString = nameSpace + ":" + name;
+        NameBytes = encoding.GetBytes(name);
+        NamespaceBytes = encoding.GetBytes(nameSpace);
+        FullNameBytes = encoding.GetBytes(FullNameString);
+    }
+
+    public override string ToString() => FullNameString;
+}
--- IT.Markuping.Tests/TagFinder/TagFinderByteTester.cs
+++ IT.Markuping.Tests/TagFinder/TagFinderByteTester.cs
@@ -18,49 +18,6 @@ internal class TagFinderByteTester
         _encoding = encoding;
     }
 
-    public class TagData
-    {
-        private byte[] NameBytes { get; }
-
-        private byte[] NamespaceBytes { get; }
-
-        private byte[] FullNameBytes { get; }
-
-        public string NameString { get; }
-
-        public string NamespaceString { get; }
-
-        public string FullNameString { get; }
-
-        public ReadOnlySpan<byte> Name => NameBytes;
-
-        public ReadOnlySpan<byte> Namespace => NamespaceBytes;
-
-        public ReadOnlySpan<byte> FullName => FullNameBytes;
-
-        public bool HasNamespace => NamespaceString.Length > 0;
-
-        public TagData(Encoding encoding, string name)
-        {
-            FullNameString = NameString = name;
-            FullNameBytes = NameBytes = encoding.GetBytes(name);
-            NamespaceString = string.Empty;
-            NamespaceBytes = [];
-        }
-
-        public TagData(Encoding encoding, string name, string nameSpace)
-        {
-            NameString = name;
-            NamespaceString = nameSpace;
-            FullNameString = nameSpace + ":" + name;
-            NameBytes = encoding.GetBytes(name);
-            NamespaceBytes = encoding.GetBytes(nameSpace);
-            FullNameBytes = encoding.GetBytes(FullNameString);
-        }
-
-        public override string ToString() => FullNameString;
-    }
-
     public void Test()
     {
         InternalTest();
