28d0815b2d46949c8c9d6eb1599e4137965066f0
Ivan Tikhonov <itikhonov@mail.com>
2025-12-23 21:44:15 +0300

    add TagFinderChar

--- IT.Markuping.Tests/SizeOfTest.cs
+++ IT.Markuping.Tests/SizeOfTest.cs
@@ -1,4 +1,5 @@
 ﻿using IT.Markuping.Encodings;
+using IT.Markuping.Implementation;
 using System.Runtime.CompilerServices;
 
 namespace IT.Markuping.Tests;
@@ -15,7 +16,9 @@ internal class SizeOfTest
         Assert.That(Unsafe.SizeOf<TagOpening>(), Is.EqualTo(8));
         Assert.That(Unsafe.SizeOf<TagClosing>(), Is.EqualTo(8));
         Assert.That(Unsafe.SizeOf<ByteEncoding.Tokens>(), Is.EqualTo(8));
-
+        Assert.That(Unsafe.SizeOf<EquatableTagFinder<byte>.Tokens>(), Is.EqualTo(8));
+        Assert.That(Unsafe.SizeOf<EquatableTagFinder<char>.Tokens>(), Is.EqualTo(16));
+        Assert.That(Unsafe.SizeOf<EquatableTagFinder<int>.Tokens>(), Is.EqualTo(32));
         Assert.That(Unsafe.SizeOf<Tags>(), Is.EqualTo(16));
     }
 }
---
+++ IT.Markuping/Implementation/TagFinder/TagFinderChar.cs
@@ -0,0 +1,21 @@
+﻿#if NET
+using System;
+
+namespace IT.Markuping.Implementation;
+
+public class TagFinderChar : EquatableTagFinder<char>
+{
+    private readonly StringComparison _comparison;
+
+    public TagFinderChar(Tokens tokens, StringComparison comparison) : base(tokens)
+    {
+        _comparison = comparison;
+    }
+
+    protected override int IndexOf(ReadOnlySpan<char> data, ReadOnlySpan<char> value)
+        => data.IndexOf(value, _comparison);
+
+    protected override int LastIndexOf(ReadOnlySpan<char> data, ReadOnlySpan<char> value)
+        => data.LastIndexOf(value, _comparison);
+}
+#endif
