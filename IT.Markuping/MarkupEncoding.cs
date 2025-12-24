1477c1fc29da0c8e07729afc7884f17ddfc69748
Ivan Tikhonov <itikhonov@mail.com>
2025-12-24 19:40:15 +0300

    MarkupEncoding

--- IT.Markuping/MarkupEncoding.cs
+++ IT.Markuping/MarkupEncoding.cs
@@ -7,13 +7,93 @@ public readonly struct MarkupEncoding<T> where T : unmanaged
     private readonly T[] _abc;
     private readonly int _size;
 
+    #region Props
+
     public int Size => _size;
 
     public bool IsComplex => _size > 1;
 
+    /// <summary>
+    /// &lt;
+    /// </summary>
     public ReadOnlySpan<T> Lt => _abc.AsSpan(0, _size);
 
+    /// <summary>
+    /// &gt;
+    /// </summary>
     public ReadOnlySpan<T> Gt => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// /
+    /// </summary>
+    public ReadOnlySpan<T> Slash => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// :
+    /// </summary>
+    public ReadOnlySpan<T> Colon => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// ' '
+    /// </summary>
+    public ReadOnlySpan<T> Space => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// "
+    /// </summary>
+    public ReadOnlySpan<T> Quot => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// '
+    /// </summary>
+    public ReadOnlySpan<T> Apos => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// =
+    /// </summary>
+    public ReadOnlySpan<T> Eq => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// !
+    /// </summary>
+    public ReadOnlySpan<T> Excl => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// -
+    /// </summary>
+    public ReadOnlySpan<T> Dash => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// [
+    /// </summary>
+    public ReadOnlySpan<T> LSQB => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// ]
+    /// </summary>
+    public ReadOnlySpan<T> RSQB => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// ?
+    /// </summary>
+    public ReadOnlySpan<T> Quest => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// \r
+    /// </summary>
+    public ReadOnlySpan<T> CR => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// \n
+    /// </summary>
+    public ReadOnlySpan<T> LF => _abc.AsSpan(0, _size);
+
+    /// <summary>
+    /// \t
+    /// </summary>
+    public ReadOnlySpan<T> Tab => _abc.AsSpan(0, _size);
+
+    #endregion Props
 
     public MarkupEncoding(int size, T[] abc)
     {
