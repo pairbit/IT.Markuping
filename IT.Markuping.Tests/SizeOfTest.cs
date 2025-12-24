5f8d85ef013f04da7cfc1e3284078236b97d4a1a
Ivan Tikhonov <itikhonov@mail.com>
2025-12-25 00:47:51 +0300

    refac

--- IT.Markuping.Tests/SizeOfTest.cs
+++ IT.Markuping.Tests/SizeOfTest.cs
@@ -1,5 +1,4 @@
-﻿using IT.Markuping.Encodings;
-using IT.Markuping.Implementation;
+﻿using IT.Markuping.Implementation;
 using System.Runtime.CompilerServices;
 
 namespace IT.Markuping.Tests;
@@ -15,7 +14,6 @@ internal class SizeOfTest
         Assert.That(Unsafe.SizeOf<Tag>(), Is.EqualTo(8));
         Assert.That(Unsafe.SizeOf<TagOpening>(), Is.EqualTo(8));
         Assert.That(Unsafe.SizeOf<TagClosing>(), Is.EqualTo(8));
-        Assert.That(Unsafe.SizeOf<ByteEncoding.Tokens>(), Is.EqualTo(8));
         Assert.That(Unsafe.SizeOf<TagFinder<byte>.Tokens>(), Is.EqualTo(8));
         Assert.That(Unsafe.SizeOf<TagFinder<char>.Tokens>(), Is.EqualTo(16));
         Assert.That(Unsafe.SizeOf<TagFinder<int>.Tokens>(), Is.EqualTo(32));
