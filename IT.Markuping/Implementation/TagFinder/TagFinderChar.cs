fb656c7c036fc9b701bd65015fa325a16bc3f63a
Ivan Tikhonov <itikhonov@mail.com>
2025-12-24 09:41:34 +0300

    TagFinderChar

--- IT.Markuping/Implementation/TagFinder/TagFinderChar.cs
+++ IT.Markuping/Implementation/TagFinder/TagFinderChar.cs
@@ -1,21 +1,33 @@
-﻿#if NET
-using System;
+﻿using System;
 
 namespace IT.Markuping.Implementation;
 
 public class TagFinderChar : EquatableTagFinder<char>
 {
+#if NET
     private readonly StringComparison _comparison;
 
     public TagFinderChar(Tokens tokens, StringComparison comparison) : base(tokens)
     {
         _comparison = comparison;
     }
+#else
+    public TagFinderChar(Tokens tokens) : base(tokens)
+    {
 
+    }
+#endif
     protected override int IndexOf(ReadOnlySpan<char> data, ReadOnlySpan<char> value)
-        => data.IndexOf(value, _comparison);
+        => data.IndexOf(value
+#if NET
+            , _comparison
+#endif
+            );
 
     protected override int LastIndexOf(ReadOnlySpan<char> data, ReadOnlySpan<char> value)
-        => data.LastIndexOf(value, _comparison);
-}
+        => data.LastIndexOf(value
+#if NET
+            , _comparison
 #endif
+            );
+}
