c1e62f015c4a2aee88a1f0d7f62573f7c00c3146
Ivan Tikhonov <itikhonov@mail.com>
2025-12-25 14:57:02 +0300

    refac

--- IT.Markuping.Tests/MarkupEncodingInfosTest.cs
+++ IT.Markuping.Tests/MarkupEncodingInfosTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace IT.Markuping.Tests;
@@ -7,7 +8,15 @@ namespace IT.Markuping.Tests;
 internal class MarkupEncodingInfosTest
 {
     [Test]
-    public void Test()
+    public void CastTest()
+    {
+        CastTest(Encoding.Unicode, MarkupEncodingByteInfos.Utf16, MarkupEncodingCharInfos.Utf16);
+        CastTest(Encoding.Unicode, MarkupEncodingByteInfos.Utf16_Strict, MarkupEncodingCharInfos.Utf16_Strict);
+        //CastTest(Encoding.BigEndianUnicode, MarkupEncodingByteInfos.Utf16BE, MarkupEncodingCharInfos.Utf16);
+    }
+
+    [Test]
+    public void ByteTest()
     {
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
@@ -22,7 +31,7 @@ internal class MarkupEncodingInfosTest
 
             if (MarkupEncodingByteInfos.TryGet(codePage, out var markupEncodingInfo))
             {
-                Test(markupEncodingInfo, encoding);
+                ByteTest(markupEncodingInfo, encoding);
             }
             else
             {
@@ -31,7 +40,7 @@ internal class MarkupEncodingInfosTest
 
             if (MarkupEncodingByteInfos.TryGetStrict(codePage, out markupEncodingInfo))
             {
-                Test(markupEncodingInfo, encoding);
+                ByteTest(markupEncodingInfo, encoding);
             }
             else
             {
@@ -40,7 +49,7 @@ internal class MarkupEncodingInfosTest
         }
     }
 
-    private void Test(MarkupEncodingInfo<byte> markupEncodingInfo, Encoding encoding)
+    private static void ByteTest(MarkupEncodingInfo<byte> markupEncodingInfo, Encoding encoding)
     {
         if (!encoding.IsSingleByte)
         {
@@ -129,4 +138,86 @@ internal class MarkupEncodingInfosTest
             }
         }
     }
+
+    private static void CastTest<T>(Encoding encoding, MarkupEncodingInfo<byte> mebi, MarkupEncodingInfo<T> meci) where T : unmanaged
+    {
+        Assert.That(mebi.CodePages.Contains(encoding.CodePage), Is.True);
+        Assert.That(meci.CodePages.Contains(encoding.CodePage), Is.True);
+
+        var meb = mebi.Encoding;
+        var mec = meci.Encoding;
+
+        var lt = Cast<T>(meb.LT);
+        var gt = Cast<T>(meb.GT);
+        var slash = Cast<T>(meb.Slash);
+        var colon = Cast<T>(meb.Colon);
+        var space = Cast<T>(meb.Space);
+        var quot = Cast<T>(meb.Quot);
+        var eq = Cast<T>(meb.Eq);
+        var apos = Cast<T>(meb.Apos);
+        var cr = Cast<T>(meb.CR);
+        var lf = Cast<T>(meb.LF);
+        var tab = Cast<T>(meb.Tab);
+
+        Assert.That(mec.LT.SequenceEqual(lt), Is.True);
+        Assert.That(mec.GT.SequenceEqual(gt), Is.True);
+        Assert.That(mec.Slash.SequenceEqual(slash), Is.True);
+        Assert.That(mec.Colon.SequenceEqual(colon), Is.True);
+        Assert.That(mec.Space.SequenceEqual(space), Is.True);
+        Assert.That(mec.Quot.SequenceEqual(quot), Is.True);
+        Assert.That(mec.Eq.SequenceEqual(eq), Is.True);
+
+        if (meb.IsStrict)
+        {
+            Assert.That(mec.IsStrict, Is.True);
+            Assert.That(mec.Apos.IsEmpty, Is.EqualTo(meb.Apos.IsEmpty));
+            Assert.That(mec.CR.IsEmpty, Is.EqualTo(meb.CR.IsEmpty));
+            Assert.That(mec.LF.IsEmpty, Is.EqualTo(meb.LF.IsEmpty));
+            Assert.That(mec.Tab.IsEmpty, Is.EqualTo(meb.Tab.IsEmpty));
+        }
+        else
+        {
+            Assert.That(mec.Apos.SequenceEqual(apos), Is.True);
+            Assert.That(mec.CR.SequenceEqual(cr), Is.True);
+            Assert.That(mec.LF.SequenceEqual(lf), Is.True);
+            Assert.That(mec.Tab.SequenceEqual(tab), Is.True);
+        }
+
+        Assert.That(meb.IsComplex, Is.True);
+
+        if (mec.IsComplex)
+        {
+            Console.WriteLine($"{encoding.CodePage} is complex");
+        }
+        else
+        {
+            var tokens = (MarkupTokens<T>)mec;
+
+            Assert.That(tokens.LT, Is.EqualTo(lt[0]));
+            Assert.That(tokens.GT, Is.EqualTo(gt[0]));
+            Assert.That(tokens.Slash, Is.EqualTo(slash[0]));
+            Assert.That(tokens.Colon, Is.EqualTo(colon[0]));
+            Assert.That(tokens.Space, Is.EqualTo(space[0]));
+            Assert.That(tokens.Quot, Is.EqualTo(quot[0]));
+            Assert.That(tokens.Eq, Is.EqualTo(eq[0]));
+
+            if (mec.IsStrict)
+            {
+                Assert.That(tokens.Apos, Is.Zero);
+                Assert.That(tokens.CR, Is.Zero);
+                Assert.That(tokens.LF, Is.Zero);
+                Assert.That(tokens.Tab, Is.Zero);
+            }
+            else
+            {
+                Assert.That(tokens.Apos, Is.EqualTo(apos[0]));
+                Assert.That(tokens.CR, Is.EqualTo(cr[0]));
+                Assert.That(tokens.LF, Is.EqualTo(lf[0]));
+                Assert.That(tokens.Tab, Is.EqualTo(tab[0]));
+            }
+        }
+    }
+
+    private static ReadOnlySpan<T> Cast<T>(ReadOnlySpan<byte> span) where T : unmanaged
+       => MemoryMarshal.Cast<byte, T>(span);
 }
