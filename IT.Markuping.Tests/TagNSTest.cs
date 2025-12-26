6ccf986bcea332a83a394300ca436f0d637f6d9d
Ivan Tikhonov <itikhonov@mail.com>
2025-12-26 18:56:02 +0300

    #5 #4 TagNS

---
+++ IT.Markuping.Tests/TagNSTest.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace IT.Markuping.Tests;
+
+internal class TagNSTest
+{
+    [Test]
+    public void CtorTest()
+    {
+        var tag = new TagNS();
+        Assert.That(tag.Start, Is.EqualTo(0));
+        Assert.That(tag.End, Is.EqualTo(0));
+        Assert.That(tag.Length, Is.EqualTo(0));
+        Assert.That(tag.ToString(), Is.EqualTo("0..0"));
+
+        tag = default;
+        Assert.That(tag.Start, Is.EqualTo(0));
+        Assert.That(tag.End, Is.EqualTo(0));
+        Assert.That(tag.Length, Is.EqualTo(0));
+
+        tag = new TagNS(0, 1);
+        Assert.That(tag.Start, Is.EqualTo(0));
+        Assert.That(tag.End, Is.EqualTo(1));
+        Assert.That(tag.Length, Is.EqualTo(1));
+        Assert.That(tag.ToString(), Is.EqualTo("0..1"));
+
+        tag = new TagNS(10, 100);
+        Assert.That(tag.Start, Is.EqualTo(10));
+        Assert.That(tag.End, Is.EqualTo(100));
+        Assert.That(tag.Length, Is.EqualTo(90));
+        Assert.That(tag.ToString(), Is.EqualTo("10..100"));
+
+        tag = new TagNS(5, 10);
+        Assert.That(tag.Start, Is.EqualTo(5));
+        Assert.That(tag.End, Is.EqualTo(10));
+        Assert.That(tag.Length, Is.EqualTo(5));
+        Assert.That(tag.ToString(), Is.EqualTo("5..10"));
+
+        tag = new TagNS(1, 3);
+        Assert.That(tag.Start, Is.EqualTo(1));
+        Assert.That(tag.End, Is.EqualTo(3));
+        Assert.That(tag.ToString(), Is.EqualTo("1..3"));
+    }
+
+    [Test]
+    public void AddOffsetTest()
+    {
+        var tag = new TagNS(0, 1);
+        Assert.That(tag.Start, Is.EqualTo(0));
+        Assert.That(tag.End, Is.EqualTo(1));
+
+        tag = tag.AddOffset(10);
+        Assert.That(tag.Start, Is.EqualTo(10));
+        Assert.That(tag.End, Is.EqualTo(11));
+
+        tag = tag.AddOffset(-10);
+        Assert.That(tag.Start, Is.EqualTo(0));
+        Assert.That(tag.End, Is.EqualTo(1));
+
+        var ex1 = Assert.Throws<OverflowException>(() => tag.AddOffset(int.MaxValue));
+
+        var ex2 = Assert.Throws<ArgumentOutOfRangeException>(() => tag.AddOffset(-1));
+        Assert.That(ex2.ParamName, Is.EqualTo("offset"));
+
+        var maxOffset = int.MaxValue - 1;
+
+        tag = tag.AddOffset(maxOffset);
+        Assert.That(tag.Start, Is.EqualTo(maxOffset));
+        Assert.That(tag.End, Is.EqualTo(maxOffset + 1));
+
+        ex2 = Assert.Throws<ArgumentOutOfRangeException>(() => tag.AddOffset(int.MinValue));
+        Assert.That(ex2.ParamName, Is.EqualTo("offset"));
+
+        tag = new TagNS(0, 1);
+        Assert.That(tag.Start, Is.EqualTo(0));
+        Assert.That(tag.End, Is.EqualTo(1));
+
+        tag = tag.AddOffset(100);
+        Assert.That(tag.Start, Is.EqualTo(100));
+        Assert.That(tag.End, Is.EqualTo(101));
+
+        tag = tag.AddOffset(-100);
+        Assert.That(tag.Start, Is.EqualTo(0));
+        Assert.That(tag.End, Is.EqualTo(1));
+
+        ex1 = Assert.Throws<OverflowException>(() => tag.AddOffset(int.MaxValue));
+
+        ex2 = Assert.Throws<ArgumentOutOfRangeException>(() => tag.AddOffset(-1));
+        Assert.That(ex2.ParamName, Is.EqualTo("offset"));
+
+        tag = tag.AddOffset(maxOffset);
+        Assert.That(tag.Start, Is.EqualTo(maxOffset));
+        Assert.That(tag.End, Is.EqualTo(maxOffset + 1));
+
+        ex2 = Assert.Throws<ArgumentOutOfRangeException>(() => tag.AddOffset(int.MinValue));
+        Assert.That(ex2.ParamName, Is.EqualTo("offset"));
+    }
+
+#if NET
+    [Test]
+    public void TryFormatTest()
+    {
+        var tag = new TagNS(10, 11);
+        Assert.That(tag.TryFormat(stackalloc char[4], out var written), Is.False);
+        Assert.That(written == 0, Is.True);
+
+        Span<char> span = stackalloc char[5];
+        Assert.That(tag.TryFormat(span, out written), Is.False);
+        Assert.That(written == 0, Is.True);
+
+        span = stackalloc char[6];
+        Assert.That(tag.TryFormat(span, out written), Is.True);
+        Assert.That(written == 6, Is.True);
+        Assert.That(span.ToString(), Is.EqualTo("10..11"));
+
+        tag = new TagNS(12345, 123456);
+        span = stackalloc char[12];
+        Assert.That(tag.TryFormat(span, out written), Is.False);
+        Assert.That(written == 0, Is.True);
+
+        span = stackalloc char[13];
+        Assert.That(tag.TryFormat(span, out written), Is.True);
+        Assert.That(written == 13, Is.True);
+        Assert.That(span.ToString(), Is.EqualTo("12345..123456"));
+    }
+#endif
+
+    [Test]
+    public void CompareToTest()
+    {
+        var tag1 = new TagNS(100, 101);
+        var tag2 = new TagNS(101, 102);
+
+        Assert.That(tag1 < tag2, Is.True);
+        Assert.That(tag2 > tag1, Is.True);
+
+        tag2 = tag1;
+        Assert.That(tag1 <= tag2, Is.True);
+        Assert.That(tag1 >= tag2, Is.True);
+    }
+
+    [Test]
+    public void InvalidTest()
+    {
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => new TagNS(-1, 0));
+        Assert.That(ex.ParamName, Is.EqualTo("start"));
+
+        ex = Assert.Throws<ArgumentOutOfRangeException>(() => new TagNS(0, 0));
+        Assert.That(ex.ParamName, Is.EqualTo("end"));
+
+        ex = Assert.Throws<ArgumentOutOfRangeException>(() => new TagNS(5, 4));
+        Assert.That(ex.ParamName, Is.EqualTo("end"));
+    }
+}
---
+++ IT.Markuping/TagNS.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace IT.Markuping;
+
+[DebuggerDisplay("{ToString(),nq}")]
+public readonly struct TagNS : IComparable<TagNS>, IEquatable<TagNS>, IFormattable
+#if NET6_0_OR_GREATER
+, ISpanFormattable
+#endif
+{
+    private readonly int _start;
+    private readonly int _end;
+
+    #region Props
+
+    public int Start => _start;
+
+    public int End => _end;
+
+    public int Length => End - Start;
+
+    public bool IsEmpty => _start == _end;
+
+    public Range Range => new(Start, End);
+
+    #endregion Props
+
+    #region Ctors
+
+    private TagNS(int start, int end, int offset)
+    {
+        _end = checked(end + offset);
+        if (_end < 1) throw new ArgumentOutOfRangeException(nameof(offset));
+
+        _start = checked(start + offset);
+        if (_start < 0) throw new ArgumentOutOfRangeException(nameof(offset));
+    }
+
+    public TagNS(int start, int end)
+    {
+        if (start < 0) throw new ArgumentOutOfRangeException(nameof(start));
+        if (end <= start) throw new ArgumentOutOfRangeException(nameof(end));
+
+        _start = start;
+        _end = end;
+    }
+
+    #endregion Ctors
+
+    public TagNS AddOffset(int offset) => new(_start, _end, offset);
+
+    public TagNS MultipleOffset(int offset)
+        => new(checked(_start * offset), checked(_end * offset));
+
+    #region Comparison
+
+    public int CompareTo(TagNS other)
+    {
+        var compared = Start.CompareTo(other.Start);
+
+        Debug.Assert(End.CompareTo(other.End) == compared);
+
+        return compared;
+    }
+
+    public bool Equals(TagNS other) => _start == other._start && _end == other._end;
+
+    public override bool Equals(object? obj) => obj is TagNS tag && Equals(tag);
+
+    public override int GetHashCode() => HashCode.Combine(_start, _end);
+
+    #endregion Comparison
+
+    #region ToString
+
+    public override string ToString()
+    {
+#if NETSTANDARD2_0
+        return $"{Start}..{End}";
+#else
+        Span<char> span = stackalloc char[2 + (2 * 10)];
+
+        var status = TryFormat(span, out var written);
+
+        Debug.Assert(status);
+
+        return new(span.Slice(0, written));
+#endif
+    }
+
+#if !NETSTANDARD2_0
+    public bool TryFormat(Span<char> chars, out int written, bool clear = true)
+    {
+        var minLength = 2;
+        //0..3
+        //min-max = 4-22
+        if (chars.Length >= minLength + 2 && ((uint)Start).TryFormat(chars, out var startWritten))
+        {
+            if (chars.Length >= minLength + startWritten + startWritten &&
+                ((uint)End).TryFormat(chars.Slice(2 + startWritten), out var endWritten))
+            {
+                written = minLength + startWritten + endWritten;
+                if (chars.Length >= written)
+                {
+                    chars[startWritten] = '.';
+                    chars[startWritten + 1] = '.';
+                    return true;
+                }
+                else if (clear)
+                {
+                    chars.Slice(0, startWritten + 2 + endWritten).Clear();
+                }
+            }
+            else if (clear)
+            {
+                chars.Slice(1, startWritten).Clear();
+            }
+        }
+        written = 0;
+        return false;
+    }
+#endif
+
+    #endregion ToString
+
+    #region Formattable
+
+    string IFormattable.ToString(string? format, IFormatProvider? formatProvider)
+        => ToString();
+
+#if NET6_0_OR_GREATER
+    bool ISpanFormattable.TryFormat(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider? provider)
+    {
+        if (format.Length != 0) throw new FormatException();
+
+        return TryFormat(destination, out charsWritten);
+    }
+#endif
+
+    #endregion Formattable
+
+    #region Operators
+
+    public static bool operator ==(TagNS left, TagNS right) => left.Equals(right);
+
+    public static bool operator !=(TagNS left, TagNS right) => !left.Equals(right);
+
+    public static bool operator <(TagNS left, TagNS right) => left.CompareTo(right) < 0;
+
+    public static bool operator <=(TagNS left, TagNS right) => left.CompareTo(right) <= 0;
+
+    public static bool operator >(TagNS left, TagNS right) => left.CompareTo(right) > 0;
+
+    public static bool operator >=(TagNS left, TagNS right) => left.CompareTo(right) >= 0;
+
+    #endregion Operators
+}
