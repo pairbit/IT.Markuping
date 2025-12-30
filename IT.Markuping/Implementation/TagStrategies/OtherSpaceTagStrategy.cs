72113ffb9c4f51c0e140e35afffa7e10afa480fa
Ivan Tikhonov <itikhonov@mail.com>
2025-12-30 14:25:14 +0300

    add

---
+++ IT.Markuping/Implementation/TagStrategies/OtherSpaceTagStrategy.cs
@@ -0,0 +1,24 @@
+﻿using IT.Markuping.Implementation.TagStrategies;
+using System;
+
+namespace IT.Markuping.Implementation;
+
+public class OtherSpaceTagStrategy<T> : TagStrategy<T> where T : unmanaged, IEquatable<T>
+{
+    private readonly T _otherSpace;
+
+    public OtherSpaceTagStrategy(Tokens tokens, T otherSpace) : base(tokens)
+    {
+        _otherSpace = otherSpace;
+    }
+
+    public OtherSpaceTagStrategy(MarkupTokens<T> tokens) : base(new(tokens))
+    {
+        if (!tokens._cr.Equals(tokens._lf) || !tokens._cr.Equals(tokens._tab)) throw new ArgumentOutOfRangeException(nameof(tokens));
+
+        _otherSpace = tokens._cr;
+    }
+
+    protected override bool IsSpace(T value)
+        => value.Equals(_tokens._space) || value.Equals(_otherSpace);
+}
---
+++ IT.Markuping/Implementation/TagStrategies/OtherSpacesTagStrategy.cs
@@ -0,0 +1,29 @@
+﻿using IT.Markuping.Implementation.TagStrategies;
+using System;
+
+namespace IT.Markuping.Implementation;
+
+public class OtherSpacesTagStrategy<T> : TagStrategy<T> where T : unmanaged, IEquatable<T>
+{
+    private readonly T _cr;//\r
+    private readonly T _lf;//\n
+    private readonly T _tab;//\t
+
+    public OtherSpacesTagStrategy(Tokens tokens, T cr, T lf, T tab) : base(tokens)
+    {
+        _cr = cr;
+        _lf = lf;
+        _tab = tab;
+    }
+
+    public OtherSpacesTagStrategy(MarkupTokens<T> tokens) : base(new(tokens))
+    {
+        _cr = tokens._cr;
+        _lf = tokens._lf;
+        _tab = tokens._tab;
+    }
+
+    protected override bool IsSpace(T value) =>
+        value.Equals(_tokens._space) || value.Equals(_lf) ||
+        value.Equals(_cr) || value.Equals(_tab);
+}
