2c70524887ba37c7f7c7b8a444a7a46a5572ff43
Ivan Tikhonov <itikhonov@mail.com>
2025-12-30 16:03:34 +0300

    add extens

--- IT.Markuping.Tests/TagFinder/TagFinderCharTest.cs
+++ IT.Markuping.Tests/TagFinder/TagFinderCharTest.cs
@@ -1,7 +1,7 @@
-﻿using IT.Markuping.Implementation;
-#if NET
+﻿#if NET
+using IT.Markuping.Extensions;
+using IT.Markuping.Implementation;
 using IT.Markuping.Interfaces;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace IT.Markuping.Tests;
@@ -13,20 +13,18 @@ internal class TagFinderCharTest
     {
         Test(TagFinderChar.Utf16_IgnoreCase);
 
-        var tags = TagFinderChar.Utf16_IgnoreCase.FirstTags("<NaMe></name>", "name", out _);
+        var tags = TagFinderChar.Utf16_IgnoreCase.FirstTags("<NaMe></name>", "name");
         Assert.That(tags.IsEmpty, Is.False);
     }
 
     private static void Test(ITagFinder<char> finder)
     {
         var data = "<NaMe></NaMe>";
-        var tags = finder.FirstTags(data, "name", out var nodes);
-        Assert.That(nodes, Is.Zero);
+        var tags = finder.FirstTags(data, "name");
         Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
 
         data = "<Ns:NaMe></Ns:NaMe>";
-        tags = finder.FirstTags(data, "name", "ns", out nodes);
-        Assert.That(nodes, Is.Zero);
+        tags = finder.FirstTags(data, "name", "ns");
         Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
     }
 }
--- IT.Markuping/Extensions/xITagFinder.cs
+++ IT.Markuping/Extensions/xITagFinder.cs
@@ -5,6 +5,24 @@ namespace IT.Markuping.Extensions;
 
 public static class xITagFinder
 {
+    public static Tags FirstTags<T>(this ITagFinder<T> finder, ReadOnlySpan<T> data, ReadOnlySpan<T> name, out TagNS ns) where T : unmanaged
+        => finder.FirstTags(data, name, out ns, out _);
+
+    public static Tags FirstTags<T>(this ITagFinder<T> finder, ReadOnlySpan<T> data, ReadOnlySpan<T> name, ReadOnlySpan<T> ns) where T : unmanaged
+        => finder.FirstTags(data, name, ns, out _);
+
+    public static Tags FirstTags<T>(this ITagFinder<T> finder, ReadOnlySpan<T> data, ReadOnlySpan<T> name) where T : unmanaged
+        => finder.FirstTags(data, name, out _);
+
+    public static Tags LastTags<T>(this ITagFinder<T> finder, ReadOnlySpan<T> data, ReadOnlySpan<T> name, out TagNS ns) where T : unmanaged
+        => finder.LastTags(data, name, out ns, out _);
+
+    public static Tags LastTags<T>(this ITagFinder<T> finder, ReadOnlySpan<T> data, ReadOnlySpan<T> name, ReadOnlySpan<T> ns) where T : unmanaged
+        => finder.LastTags(data, name, ns, out _);
+
+    public static Tags LastTags<T>(this ITagFinder<T> finder, ReadOnlySpan<T> data, ReadOnlySpan<T> name) where T : unmanaged
+        => finder.LastTags(data, name, out _);
+
     public static TagOpening FirstTagOpening<T>(this ITagFinder<T> finder, ReadOnlySpan<T> data, ReadOnlySpan<T> name, ReadOnlySpan<T> ns, TagEndings endings = default) where T : unmanaged
         => (TagOpening)finder.FirstTag(data, name, ns, endings.GetEndings());
 
