fa44a8c0722ad87f6166c8d1aaff68387cdf20c4
Ivan Tikhonov <itikhonov@mail.com>
2026-01-02 18:36:47 +0300

    add TagStrategyByte

---
+++ IT.Markuping/Implementation/TagStrategies/TagStrategyByte.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace IT.Markuping.Implementation.TagStrategies;
+
+public class TagStrategyByte : TagStrategy<byte>
+{
+    private readonly bool[] _otherSpaces;
+
+    public ReadOnlySpan<bool> OtherSpaces => _otherSpaces;
+
+    public TagStrategyByte(Tokens tokens, bool[] otherSpaces) : base(tokens)
+    {
+        if (otherSpaces == null) throw new ArgumentNullException(nameof(otherSpaces));
+        if (otherSpaces.Length != 256) throw new ArgumentOutOfRangeException(nameof(otherSpaces));
+
+        _otherSpaces = otherSpaces;
+    }
+
+    protected override bool IsSpace(byte value)
+    {
+        Debug.Assert(_otherSpaces.Length == 256);
+
+        return _otherSpaces[value];
+    }
+}
