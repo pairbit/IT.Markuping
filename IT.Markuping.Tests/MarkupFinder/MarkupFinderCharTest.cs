8564ea848daf971dbf8dce104e83e4a479670d0c
Ivan Tikhonov <itikhonov@mail.com>
2026-01-04 14:47:15 +0300

    tests

--- IT.Markuping.Tests/MarkupFinder/MarkupFinderCharTest.cs
+++ IT.Markuping.Tests/MarkupFinder/MarkupFinderCharTest.cs
@@ -11,9 +11,6 @@ internal class MarkupFinderCharTest
     public void Test()
     {
         Test(MarkupFinders.Char.Utf16_IgnoreCase);
-
-        var tags = MarkupFinders.Char.Utf16_IgnoreCase.FirstTags("<NaMe></name>", "name");
-        Assert.That(tags.IsEmpty, Is.False);
     }
 
     private static void Test(IMarkupFinder<char> finder)
@@ -22,7 +19,15 @@ internal class MarkupFinderCharTest
         var tags = finder.FirstTags(data, "name");
         Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
 
+        data = "<NaMe></name>";
+        tags = finder.FirstTags(data, "name");
+        Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
+
         data = "<Ns:NaMe></Ns:NaMe>";
+        tags = finder.FirstTags(data, "name", "ns");
+        Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
+
+        data = "<Ns:NaMe></NS:name>";
         tags = finder.FirstTags(data, "name", "ns");
         Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
     }
