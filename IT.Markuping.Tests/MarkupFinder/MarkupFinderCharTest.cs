756f8d7c351da06c8ebc34ede980cd857e1aa125
Ivan Tikhonov <itikhonov@mail.com>
2026-04-20 14:31:11 +0300

    MarkupFinderCharTest

--- IT.Markuping.Tests/MarkupFinder/MarkupFinderCharTest.cs
+++ IT.Markuping.Tests/MarkupFinder/MarkupFinderCharTest.cs
@@ -2,6 +2,7 @@
 using IT.Markuping.Extensions;
 using IT.Markuping.Interfaces;
 using System.Linq;
+using System.Text;
 
 namespace IT.Markuping.Tests;
 
@@ -11,6 +12,7 @@ internal class MarkupFinderCharTest
     public void Test()
     {
         Test(MarkupFinders.Char.Utf16_IgnoreCase);
+        Test(MarkupFinders.Utf16_IgnoreCase, Encoding.Unicode);
     }
 
     private static void Test(IMarkupFinder<char> finder)
@@ -30,6 +32,30 @@ internal class MarkupFinderCharTest
         data = "<Ns:NaMe></NS:name>";
         tags = finder.FirstTags(data, "name", "ns");
         Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
+    }
+
+    private static void Test(IMarkupFinder<byte> finder, Encoding encoding)
+    {
+        var name = encoding.GetBytes("name");
+        var ns = encoding.GetBytes("ns");
+
+        var data = encoding.GetBytes("<NaMe></NaMe>");
+        var tags = finder.FirstTags(data, name);
+        Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
+
+        data = encoding.GetBytes("<NaMe></name>");
+        tags = finder.FirstTags(data, name);
+        Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
+
+        data = encoding.GetBytes("<Ns:NaMe></Ns:NaMe>");
+        tags = finder.FirstTags(data, name, ns);
+        Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
+
+        data = encoding.GetBytes("<Ns:NaMe></NS:name>");
+        tags = finder.FirstTags(data, name, ns);
+        Assert.That(data[tags.Outer].SequenceEqual(data), Is.True);
+
+        Assert.That(finder.FirstTags(data, encoding.GetBytes("invalid"), ns).IsEmpty, Is.True);
     }
 }
 #endif
