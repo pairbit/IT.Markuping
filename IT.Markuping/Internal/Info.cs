e45a9c4c2695a548f252fccae8659085ad9154e5
Ivan Tikhonov <itikhonov@mail.com>
2026-04-21 11:52:01 +0300

    info bytes

--- IT.Markuping/Internal/Info.cs
+++ IT.Markuping/Internal/Info.cs
@@ -1,5 +1,6 @@
 ﻿#if NET || NETSTANDARD2_1_OR_GREATER
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -22,6 +23,57 @@ internal static class Info
 
     public static string ToString(ReadOnlySpan<byte> span)
     {
+        var length = span.Length;
+        if (length == 0)
+            return string.Empty;
+
+        if ((length % 2) != 0) 
+            return Encoding.UTF8.GetString(span);//or EBCDIC
+
+        if (length == 2)
+        {
+            //Utf16BE - 00 32
+            if (span[0] == 0)
+                return Encoding.BigEndianUnicode.GetString(span);
+
+            //Utf16 - 32 00
+            if (span[1] == 0)
+                return Encoding.Unicode.GetString(span);
+
+            return Encoding.UTF8.GetString(span);
+        }
+
+        Debug.Assert(length >= 4);
+
+        //Utf16BE - 00 32 00 20
+        //Utf32BE - 00 00 00 32
+        if (span[0] == 0)
+        {
+            //Utf32BE - 00 00 00 32
+            if (span[1] == 0)
+            {
+                Debug.Assert(span[2] == 0 && span[3] != 0);
+                return Utf32BE.GetString(span);
+            }
+            //Utf16BE - 00 32 00 20
+            Debug.Assert(span[2] == 0 && span[3] != 0);
+            return Encoding.BigEndianUnicode.GetString(span);
+        }
+        //Utf16 - 32 00 20 00
+        //Utf32 - 32 00 00 00
+        else if (span[1] == 0)
+        {
+            //Utf32 - 32 00 00 00
+            if (span[2] == 0)
+            {
+                Debug.Assert(span[3] == 0);
+                return Encoding.UTF32.GetString(span);
+            }
+            //Utf16 - 32 00 20 00
+            Debug.Assert(span[3] == 0);
+            return Encoding.Unicode.GetString(span);
+        }
+
         return Encoding.UTF8.GetString(span);
     }
 
